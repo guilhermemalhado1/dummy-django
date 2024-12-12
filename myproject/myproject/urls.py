4ce0db361042ea37b76e6e0bc28e51cabe6bd38e
Guilherme Malhado <74466854+guilhermemalhado1@users.noreply.github.com>
2024-12-12 12:25:41 +0000

    Changed default html

--- myproject/myproject/urls.py
+++ myproject/myproject/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from views import home_view
 
 urlpatterns = [
     path('', home_view, name='home'),
