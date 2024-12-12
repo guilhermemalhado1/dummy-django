303c1473295d746905ae9576081901b7cf36b8f4
Guilherme Malhado <74466854+guilhermemalhado1@users.noreply.github.com>
2024-12-12 13:39:55 +0000

    Changed initial text

--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -1,4 +1,4 @@
 from django.http import HttpResponse
 
 def home_view(request,*args, **kwargs):
-    return HttpResponse("<h1>Hello World12345</h1>")
+    return HttpResponse("<h1>Hello World from Tese Mestrado</h1>")
