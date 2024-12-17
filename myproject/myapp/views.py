0cdbe1771393cf5203ea13164431d6df049ef035
Guilherme Malhado <74466854+guilhermemalhado1@users.noreply.github.com>
2024-12-17 19:12:04 +0000

    Changed initial text

--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -1,4 +1,4 @@
 from django.http import HttpResponse
 
 def home_view(request,*args, **kwargs):
-    return HttpResponse("<h1>Ola , Hoje dia 14 eu vou fazer deploy da minha aplicacao web</h1>")
+    return HttpResponse("<h1>Hoje estou a apresentar a minha tese de mestrado. Guilherme 17/12/2024</h1>")
