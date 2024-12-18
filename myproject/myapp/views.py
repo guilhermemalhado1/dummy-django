b3a83bb98d5cea6713bd07f4ae55c5666c95deeb
Guilherme Malhado <74466854+guilhermemalhado1@users.noreply.github.com>
2024-12-18 13:46:49 +0000

    Changed initial text

--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse
 
 def home_view(request,*args, **kwargs):
-    return HttpResponse("<h1>Hello World."
+    return HttpResponse("<h1>Hello World from final master degree presentation."
                         "<img src=\"https://upload.wikimedia.org/wikipedia/commons/c/c8/Log%C3%B3tipo_do_Politecnico_de_Setubal.png\" alt=\"IPS\" width=\"500\" height=\"600\"></h1>")
